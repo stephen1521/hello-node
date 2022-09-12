a6a9a2286794e15e27630dad7295bae617783fe3
stephen1521 <stephen.warnock@codeimmersives.com>
2022-09-12 14:18:39 -0400

    Added main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+let h1 = "HH"; let h2 = "HHHHHHH";
+let e1 = "e"; let e2 = "ee"; let e3 = "eee"; let e4 = "eeeee"
+let l1 = "lll";
+let o1 = "oo"; let o2 = "oooo";
+let w1 = "ww";
+let r1 = "r"; let r2 = "rr"; let r3 = "rrr";
+let d1 = "dd"; let d2 = "dddddd";
+let ex = "!!!";
+console.log("Hello World");
+console.log(h1+"   "+h1+"        "+l1+" "+l1+"                                 "+l1+"      "+d1+" "+ex);
+console.log(h1+"   "+h1+"   "+e3+"  "+l1+" "+l1+"  "+o2+"  "+w1+"      "+w1+"  "+o2+"  "+r2+" "+r2+"  "+l1+"      "+d1+" "+ex);
+console.log(h2+" "+e2+"   "+e1+" "+l1+" "+l1+" "+o1+"  "+o1+" "+w1+"      "+w1+" "+o1+"  "+o1+" "+r3+"  "+r1+" "+l1+"  "+d2+" "+ex);
+console.log(h1+"   "+h1+" "+e4+"  "+l1+" "+l1+" "+o1+"  "+o1+"  "+w1+" "+w1+" "+w1+"  "+o1+"  "+o1+" "+r2+"     "+l1+" "+d1+"   "+d1+" ");
+console.log(h1+"   "+h1+"  "+e4+" "+l1+" "+l1+"  "+o2+"    "+w1+"  "+w1+"    "+o2+"  "+r2+"     "+l1+"  "+d2+" "+ex);
